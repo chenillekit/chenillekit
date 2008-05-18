bd0dda7c96704d31b5698589dab325222c904ba2
Sven Homburgs <homburgs@gmail.com>
2008-05-18 10:36:04 +0000

    initial import

---
+++ hivemind/src/test/java/org/chenillekit/hivemind/services/impl/SimpleServiceImpl.java
@@ -0,0 +1,24 @@
+/*
+ * $Id$
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) Massimo Lusetti <mlusetti@gmail.com>.  All rights reserved.
+ *
+ * The software in this package is published under the terms of the BSD style
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.chenillekit.hivemind.services.impl;
+
+import org.chenillekit.hivemind.services.SimpleService;
+
+/**
+ * @author <a href="mailto:mlusetti@gmail.com">Massimo Lusetti</a>
+ * @version $Id$
+ */
+public class SimpleServiceImpl implements SimpleService
+{
+    public void verySimpleMethod()
+    {
+        // DO Nothing...
+    }
+}
