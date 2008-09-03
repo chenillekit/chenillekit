7d89075bd57270ddd48c0fae4d78e0dfcc8bf3e3
Sven Homburgs <homburgs@gmail.com>
2008-09-03 20:37:02 +0000

    make code more smart ;-)

--- chenillekit-tapestry/src/main/java/org/chenillekit/tapestry/core/services/impl/ThumbNailServiceImpl.java
+++ chenillekit-tapestry/src/main/java/org/chenillekit/tapestry/core/services/impl/ThumbNailServiceImpl.java
@@ -19,10 +19,6 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.net.URL;
 import java.util.zip.CRC32;
 import java.util.zip.Checksum;
 
@@ -33,7 +29,6 @@ import org.apache.tapestry5.ioc.internal.util.InternalUtils;
 import org.apache.tapestry5.services.AssetFactory;
 import org.apache.tapestry5.services.Context;
 
-import org.chenillekit.core.resources.URIResource;
 import org.chenillekit.image.services.ImageService;
 import org.chenillekit.tapestry.core.ChenilleKitCoreConstants;
 import org.chenillekit.tapestry.core.services.ThumbNailService;
@@ -45,45 +40,25 @@ import org.slf4j.Logger;
  */
 public class ThumbNailServiceImpl implements ThumbNailService
 {
-    private Logger _logger;
-    private ImageService _imageService;
-    private Resource _thumbnailDirectory;
-    private Context _context;
-    private AssetFactory _assetFactory;
+    private Logger logger;
+    private ImageService imageService;
+    private File thumbnailDirectory;
+    private Context context;
+    private AssetFactory assetFactory;
 
     public ThumbNailServiceImpl(Logger logger, ImageService imageService, Context context, AssetFactory assetFactory)
     {
-        _logger = logger;
-        _imageService = imageService;
-        _context = context;
-        _assetFactory = assetFactory;
+        this.logger = logger;
+        this.imageService = imageService;
+        this.context = context;
+        this.assetFactory = assetFactory;
 
-        try
-        {
-            URL url = context.getResource("/" + ChenilleKitCoreConstants.__THUMBNAL_DIRECTORY__);
-            File baseDir = new File(url.toURI());
-            try
-            {
-                if (!baseDir.exists())
-                    baseDir.createNewFile();
-            }
-            catch (IOException e)
-            {
-                throw new RuntimeException(e);
-            }
-            _thumbnailDirectory = new URIResource(url.toURI());
-        }
-        catch (URISyntaxException e)
-        {
-            throw new RuntimeException(e);
-        }
-        catch (MalformedURLException e)
-        {
-            throw new RuntimeException(e);
-        }
+        thumbnailDirectory = context.getRealFile(ChenilleKitCoreConstants.__THUMBNAL_DIRECTORY__);
+        if (!thumbnailDirectory.exists())
+            throw new RuntimeException("path: '" + thumbnailDirectory.getAbsolutePath() + "' not found.");
 
-        if (_logger.isDebugEnabled())
-            _logger.debug("thumbnail location: {}", _thumbnailDirectory.getPath());
+        if (this.logger.isDebugEnabled())
+            this.logger.debug("thumbnail location: {}", thumbnailDirectory.getPath());
     }
 
     /**
@@ -97,40 +72,35 @@ public class ThumbNailServiceImpl implements ThumbNailService
      */
     public Asset convertToThumbnail(Asset originalAsset, int height, float quality)
     {
-        Resource thumbRes = null;
         Asset thumbnailAsset;
         FileOutputStream output = null;
         String thumbnailFileName = String.valueOf(buildChecksumFromURI(originalAsset.toClientURL(), height, quality).getValue());
 
         try
         {
-            if (_logger.isDebugEnabled())
-                _logger.debug("original image: {}", originalAsset.toClientURL());
+            if (logger.isDebugEnabled())
+                logger.debug("original image: {}", originalAsset.toClientURL());
 
-            File outputFile = new File(new URI(_thumbnailDirectory.getPath() + "/" + thumbnailFileName));
+            File outputFile = new File(thumbnailDirectory.getAbsolutePath() + "/" + thumbnailFileName);
             if (!outputFile.exists())
             {
                 output = new FileOutputStream(outputFile, false);
-                BufferedImage bufferedImage = _imageService.scaleImage(originalAsset.getResource(), height);
-                _imageService.reduceImageQuality(bufferedImage, quality, output);
+                BufferedImage bufferedImage = imageService.scaleImage(originalAsset.getResource(), height);
+                imageService.reduceImageQuality(bufferedImage, quality, output);
                 output.flush();
             }
             else
             {
-                if (_logger.isDebugEnabled())
-                    _logger.debug("cached thumbnail '{}'", outputFile.toURL().toExternalForm());
+                if (logger.isDebugEnabled())
+                    logger.debug("cached thumbnail '{}'", outputFile.toURL().toExternalForm());
             }
 
-            thumbRes = new ContextResource(_context, ChenilleKitCoreConstants.__THUMBNAL_DIRECTORY__ + "/" + thumbnailFileName);
+            Resource thumbRes = new ContextResource(context, ChenilleKitCoreConstants.__THUMBNAL_DIRECTORY__ + "/" + thumbnailFileName);
 
-            if (_logger.isDebugEnabled())
-                _logger.debug("thumbnailed image: {}", thumbRes.toURL().toExternalForm());
+            if (logger.isDebugEnabled())
+                logger.debug("thumbnailed image: {}", thumbRes.toURL().toExternalForm());
 
-            return _assetFactory.createAsset(thumbRes);
-        }
-        catch (URISyntaxException e)
-        {
-            throw new RuntimeException(e);
+            return assetFactory.createAsset(thumbRes);
         }
         catch (FileNotFoundException e)
         {
@@ -154,14 +124,7 @@ public class ThumbNailServiceImpl implements ThumbNailService
      */
     public String getThumbnailPath()
     {
-        try
-        {
-            return _thumbnailDirectory.toURL().toURI().toString();
-        }
-        catch (URISyntaxException e)
-        {
-            throw new RuntimeException(e);
-        }
+        return thumbnailDirectory.getAbsolutePath();
     }
 
     /**
