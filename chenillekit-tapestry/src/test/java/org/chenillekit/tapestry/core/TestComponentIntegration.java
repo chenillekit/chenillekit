09932a4ea1dc0d89169590a89c755dd471d6aae7
Sven Homburgs <homburgs@gmail.com>
2009-03-28 01:36:35 +0000

    set path to FF binary because i use FF3 that dont supported by selenium

--- chenillekit-tapestry/src/test/java/org/chenillekit/tapestry/core/TestComponentIntegration.java
+++ chenillekit-tapestry/src/test/java/org/chenillekit/tapestry/core/TestComponentIntegration.java
@@ -23,213 +23,221 @@ import org.testng.annotations.Test;
  */
 public class TestComponentIntegration extends AbstractIntegrationTestSuite
 {
-    /**
-     * Initializes the suite using {@link #DEFAULT_WEB_APP_ROOT}.
-     */
-    public TestComponentIntegration()
-    {
-        super("src/test/webapp", "*firefox /usr/lib/firefox/firefox-2-bin");
-    }
+	private static String pathToFFBinary;
 
-    @Test(enabled = false)
-    public void test_accordion() throws InterruptedException
-    {
-        open(BASE_URL);
+	static
+	{
+		String userName = System.getProperty("user.name");
+		pathToFFBinary = userName.equalsIgnoreCase("sven") ? "*firefox /usr/lib/firefox/firefox-2-bin" : "*firefox";
+	}
 
-        start("Accordion");
-        waitForPageToLoad("5000");
-        click("xpath=//div[@id='accordion_toggle_1']");
-        Thread.sleep(2000);
-        assertEquals(getElementHeight("xpath=//div[@id='accordion_content_0']"), 0);
-        assertEquals(getElementHeight("xpath=//div[@id='accordion_content_1']"), 20);
+	/**
+	 * Initializes the suite using {@link #DEFAULT_WEB_APP_ROOT}.
+	 */
+	public TestComponentIntegration()
+	{
+		super("src/test/webapp", pathToFFBinary);
+	}
 
-        click("xpath=//div[@id='accordion_toggle_3']");
-        Thread.sleep(2000);
-        assertEquals(getElementHeight("xpath=//div[@id='accordion_content_3']"), 20);
-        assertEquals(getElementHeight("xpath=//div[@id='accordion_content_1']"), 0);
-    }
+	@Test(enabled = false)
+	public void test_accordion() throws InterruptedException
+	{
+		open(BASE_URL);
 
-    @Test
-    public void test_datetimefield() throws InterruptedException
-    {
-        open(BASE_URL);
+		start("Accordion");
+		waitForPageToLoad("5000");
+		click("xpath=//div[@id='accordion_toggle_1']");
+		Thread.sleep(2000);
+		assertEquals(getElementHeight("xpath=//div[@id='accordion_content_0']"), 0);
+		assertEquals(getElementHeight("xpath=//div[@id='accordion_content_1']"), 20);
 
-        start("DateTimeField");
-        waitForPageToLoad("5000");
+		click("xpath=//div[@id='accordion_toggle_3']");
+		Thread.sleep(2000);
+		assertEquals(getElementHeight("xpath=//div[@id='accordion_content_3']"), 20);
+		assertEquals(getElementHeight("xpath=//div[@id='accordion_content_1']"), 0);
+	}
 
-        type("xpath=//input[@id='dateTimeField1']", "10-12-2008 15:12");
-        type("xpath=//input[@id='dateTimeField2']", "11/31/2007");
-        clickAndWait(SUBMIT);
+	@Test
+	public void test_datetimefield() throws InterruptedException
+	{
+		open(BASE_URL);
 
-        assertEquals(getValue("xpath=//input[@id='dateTimeField1']"), "10-12-2008 15:12");
-        assertEquals(getValue("xpath=//input[@id='dateTimeField2']"), "12/01/2007");
-        assertEquals(getValue("xpath=//input[@id='dateTimeField3']"), "");
-    }
+		start("DateTimeField");
+		waitForPageToLoad("5000");
 
-    @Test
-    public void test_formater() throws InterruptedException
-    {
-        open(BASE_URL);
+		type("xpath=//input[@id='dateTimeField1']", "10-12-2008 15:12");
+		type("xpath=//input[@id='dateTimeField2']", "11/31/2007");
+		clickAndWait(SUBMIT);
 
-        start("Formater");
-        waitForPageToLoad("5000");
+		assertEquals(getValue("xpath=//input[@id='dateTimeField1']"), "10-12-2008 15:12");
+		assertEquals(getValue("xpath=//input[@id='dateTimeField2']"), "12/01/2007");
+		assertEquals(getValue("xpath=//input[@id='dateTimeField3']"), "");
+	}
 
-        assertEquals(getText("xpath=//div[@id='test1']"), "today is 01.01.1970");
-        assertEquals(getText("xpath=//div[@id='test2']"), "01/01/1970 is an US formated date");
-        assertEquals(getText("xpath=//div[@id='test3']"), "01/01/1970");
-        assertEquals(getText("xpath=//span[@id='test4']"), "this ist a very long...");
-        assertEquals(getText("xpath=//span[@id='test5']"), "this ist a...");
-        assertEquals(getText("xpath=//span[@id='test6']"), "...ery long test string");
-        assertEquals(getText("xpath=//span[@id='test7']"), "555-685458-0");
-        assertEquals(getText("xpath=//span[@id='test8']"), "555-6854__-_");
-    }
+	@Test
+	public void test_formater() throws InterruptedException
+	{
+		open(BASE_URL);
 
-    @Test
-    public void test_contains() throws InterruptedException
-    {
-        open(BASE_URL);
+		start("Formater");
+		waitForPageToLoad("5000");
 
-        start("Contains");
-        waitForPageToLoad("5000");
+		assertEquals(getText("xpath=//div[@id='test1']"), "today is 01.01.1970");
+		assertEquals(getText("xpath=//div[@id='test2']"), "01/01/1970 is an US formated date");
+		assertEquals(getText("xpath=//div[@id='test3']"), "01/01/1970");
+		assertEquals(getText("xpath=//span[@id='test4']"), "this ist a very long...");
+		assertEquals(getText("xpath=//span[@id='test5']"), "this ist a...");
+		assertEquals(getText("xpath=//span[@id='test6']"), "...ery long test string");
+		assertEquals(getText("xpath=//span[@id='test7']"), "555-685458-0");
+		assertEquals(getText("xpath=//span[@id='test8']"), "555-6854__-_");
+	}
 
-        assertEquals(getText("xpath=//div[@id='test1']"), "list contains 'test1'");
-        assertEquals(getText("xpath=//div[@id='test4']"), "list doesnt contains 'test6'");
-    }
+	@Test
+	public void test_contains() throws InterruptedException
+	{
+		open(BASE_URL);
 
-    @Test
-    public void test_equals() throws InterruptedException
-    {
-        open(BASE_URL);
+		start("Contains");
+		waitForPageToLoad("5000");
 
-        start("Equals");
-        waitForPageToLoad("5000");
+		assertEquals(getText("xpath=//div[@id='test1']"), "list contains 'test1'");
+		assertEquals(getText("xpath=//div[@id='test4']"), "list doesnt contains 'test6'");
+	}
 
-        assertEquals(getText("xpath=//div[@id='test2']"), "'testLeft' dont equals 'testRight'");
-        assertEquals(getText("xpath=//div[@id='test3']"), "'testLeft' dont equals 'testRight'");
-    }
+	@Test
+	public void test_equals() throws InterruptedException
+	{
+		open(BASE_URL);
 
-    @Test(enabled = false)
-    public void test_fieldset() throws InterruptedException
-    {
-        String style;
+		start("Equals");
+		waitForPageToLoad("5000");
 
-        open(BASE_URL);
+		assertEquals(getText("xpath=//div[@id='test2']"), "'testLeft' dont equals 'testRight'");
+		assertEquals(getText("xpath=//div[@id='test3']"), "'testLeft' dont equals 'testRight'");
+	}
 
-        start("FieldSet");
-        waitForPageToLoad("5000");
-        click("xpath=//fieldset[@id='fieldSet1']//legend");
-        Thread.sleep(2000);
-        style = getAttribute("xpath=//fieldset[@id='fieldSet1']//legend[@class='ck_fieldset_content']@style");
-        assertEquals(style, "width: 375px;");
+	@Test(enabled = false)
+	public void test_fieldset() throws InterruptedException
+	{
+		String style;
 
-        click("xpath=//fieldset[@id='fieldSet2']//legend");
-        Thread.sleep(2000);
-        style = getAttribute("xpath=//fieldset[@id='fieldSet1']@style");
-        assertEquals(style, "xxxx");
-    }
+		open(BASE_URL);
 
-    @Test
-    public void test_element() throws InterruptedException
-    {
-        open(BASE_URL);
+		start("FieldSet");
+		waitForPageToLoad("5000");
+		click("xpath=//fieldset[@id='fieldSet1']//legend");
+		Thread.sleep(2000);
+		style = getAttribute("xpath=//fieldset[@id='fieldSet1']//legend[@class='ck_fieldset_content']@style");
+		assertEquals(style, "width: 375px;");
 
-        start("Element");
-        waitForPageToLoad("5000");
+		click("xpath=//fieldset[@id='fieldSet2']//legend");
+		Thread.sleep(2000);
+		style = getAttribute("xpath=//fieldset[@id='fieldSet1']@style");
+		assertEquals(style, "xxxx");
+	}
 
-        assertEquals(getText("xpath=//strong[@id='element']"), "BlaBla");
-    }
+	@Test
+	public void test_element() throws InterruptedException
+	{
+		open(BASE_URL);
 
-    @Test
-    public void test_hidden() throws InterruptedException
-    {
-        open(BASE_URL);
+		start("Element");
+		waitForPageToLoad("5000");
 
-        start("Hidden");
-        waitForPageToLoad("5000");
+		assertEquals(getText("xpath=//strong[@id='element']"), "BlaBla");
+	}
 
-        this.runScript("document.form.hidden1.value = 'BlaBla';");
-        this.runScript("document.form.hidden2.value = '200';");
-        this.runScript("document.form.hidden3.value = '200.12';");
-        clickAndWait(SUBMIT);
+	@Test
+	public void test_hidden() throws InterruptedException
+	{
+		open(BASE_URL);
 
-        assertEquals(getText("xpath=//strong[@id='hiddenResult1']"), "BlaBla");
-        assertEquals(getText("xpath=//strong[@id='hiddenResult2']"), "200");
-        assertEquals(getText("xpath=//strong[@id='hiddenResult3']"), "200.12");
-    }
+		start("Hidden");
+		waitForPageToLoad("5000");
 
-    @Test
-    public void test_inplace() throws InterruptedException
-    {
-        open(BASE_URL);
+		this.runScript("document.form.hidden1.value = 'BlaBla';");
+		this.runScript("document.form.hidden2.value = '200';");
+		this.runScript("document.form.hidden3.value = '200.12';");
+		clickAndWait(SUBMIT);
 
-        start("InPlace");
-        waitForPageToLoad("5000");
+		assertEquals(getText("xpath=//strong[@id='hiddenResult1']"), "BlaBla");
+		assertEquals(getText("xpath=//strong[@id='hiddenResult2']"), "200");
+		assertEquals(getText("xpath=//strong[@id='hiddenResult3']"), "200.12");
+	}
 
-        click("xpath=//input[@id='inPlaceCheckbox']");
+	@Test
+	public void test_inplace() throws InterruptedException
+	{
+		open(BASE_URL);
+
+		start("InPlace");
+		waitForPageToLoad("5000");
+
+		click("xpath=//input[@id='inPlaceCheckbox']");
 //        click("xpath=//span[@id='inPlaceEditor']");
 //        type("xpath=//form[@id='inPlaceEditor-inplaceeditor']//input", "BlaBla");
 //        Thread.sleep(5000);
 //        click("xpath=//form[@id='inPlaceEditor-inplaceeditor']//input[@class='editor_ok_button']");
-        Thread.sleep(1000);
+		Thread.sleep(1000);
 
-        assertEquals(getText("xpath=//strong[@id='inPlaceCheckboxValue']"), "checked");
+		assertEquals(getText("xpath=//strong[@id='inPlaceCheckboxValue']"), "checked");
 //        assertEquals(getText("xpath=//span[@id='inPlaceEditor']"), "BlaBla");
-    }
+	}
 
-    @Test
-    public void test_thumbnail()
-    {
-        open(BASE_URL);
+	@Test
+	public void test_thumbnail()
+	{
+		open(BASE_URL);
 
-        start("ThumbNail");
-        waitForPageToLoad("5000");
-        captureScreenshot("test_thumbnail.png");
-        assertAttribute("xpath=//img@src", "thumbnails/2373045207");
+		start("ThumbNail");
+		waitForPageToLoad("5000");
+		captureScreenshot("test_thumbnail.png");
+		assertAttribute("xpath=//img@src", "thumbnails/2373045207");
 
-        click("xpath=//img[@id='thumbNail']");
-        assertAttribute("xpath=//img[@id='thumbNail']@src", "assets/images/sven.jpg");
+		click("xpath=//img[@id='thumbNail']");
+		assertAttribute("xpath=//img[@id='thumbNail']@src", "assets/images/sven.jpg");
 
-        mouseOut("xpath=//img[@id='thumbNail']");
-        assertAttribute("xpath=//img[@id='thumbNail']@src", "http://localhost:9999/thumbnails/2373045207");
-    }
+		mouseOut("xpath=//img[@id='thumbNail']");
+		assertAttribute("xpath=//img[@id='thumbNail']@src", "http://localhost:9999/thumbnails/2373045207");
+	}
 
-    @Test
-    public void test_onevent() throws InterruptedException
-    {
-        open(BASE_URL);
+	@Test
+	public void test_onevent() throws InterruptedException
+	{
+		open(BASE_URL);
 
-        start("OnEvent");
-        waitForPageToLoad("5000");
-        select("xpath=//select[@id='select1']", "BLACK");
-        select("xpath=//select[@id='select1']", "GREEN");
-        Thread.sleep(1000);
+		start("OnEvent");
+		waitForPageToLoad("5000");
+		select("xpath=//select[@id='select1']", "BLACK");
+		select("xpath=//select[@id='select1']", "GREEN");
+		Thread.sleep(1000);
 
-        assertEquals(getText("xpath=//div[@id='result1']"), "GREEN");
-    }
+		assertEquals(getText("xpath=//div[@id='result1']"), "GREEN");
+	}
 
-    @Test
-    public void test_tabset()
-    {
-        open(BASE_URL);
+	@Test
+	public void test_tabset()
+	{
+		open(BASE_URL);
 
-        start("TabSet");
-        waitForPageToLoad("5000");
+		start("TabSet");
+		waitForPageToLoad("5000");
 
-        click("xpath=//span[@id='stuff2']");
+		click("xpath=//span[@id='stuff2']");
 
-        assertTrue(this.isVisible("xpath=//div[@id='contentZone']"));
-    }
+		assertTrue(this.isVisible("xpath=//div[@id='contentZone']"));
+	}
 
-    @Test(enabled = false)
-    public void test_slideshow()
-    {
-        open(BASE_URL);
+	@Test(enabled = false)
+	public void test_slideshow()
+	{
+		open(BASE_URL);
 
-        start("SlideShow");
-        waitForPageToLoad("5000");
+		start("SlideShow");
+		waitForPageToLoad("5000");
 
-        assertTrue(this.isVisible("xpath=//img[@class='ck-slide']"));
-    }
+		assertTrue(this.isVisible("xpath=//img[@class='ck-slide']"));
+	}
 
 	@Test
 	public void test_uriasset()
