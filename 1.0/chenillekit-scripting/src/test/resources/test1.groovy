66ed2bdf2bc6e94f9d60cefdfa1d265c9ad6555c
Sven Homburgs <homburgs@gmail.com>
2009-05-08 10:20:37 +0000

    tagging release 1.0.2

--- 1.0/chenillekit-scripting/src/test/resources/test1.groovy
+++
@@ -1,9 +0,0 @@
-def amPM = Calendar.getInstance().get(Calendar.AM_PM)
-if (amPM == Calendar.AM)
-{
-	println("Good morning")
-} else {
-	println("Good evening")
-}
-
-return "Hello World from 'test1.groovy' script";
--- 1.0/chenillekit-scripting/src/test/resources/test2.groovy
+++
@@ -1,8 +0,0 @@
-registry = bsf.lookupBean('registry');
-
-x = "Hello World from 'test2.groovy' script";
-y = x;
-
-print(registry.getClass().getName());
-
-return y;
