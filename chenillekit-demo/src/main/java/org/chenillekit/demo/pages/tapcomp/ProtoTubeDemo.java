c9c42592172bc5ca971604b0acb701f896e82342
Sven Homburgs <homburgs@gmail.com>
2009-11-11 01:00:54 +0000

    add demo for ProtoTube component

--- chenillekit-demo/src/main/java/org/chenillekit/demo/pages/tapcomp/ProtoTubeDemo.java
+++ chenillekit-demo/src/main/java/org/chenillekit/demo/pages/tapcomp/ProtoTubeDemo.java
@@ -17,11 +17,8 @@ package org.chenillekit.demo.pages.tapcomp;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.apache.tapestry5.ComponentResources;
 import org.apache.tapestry5.annotations.Component;
-import org.apache.tapestry5.annotations.Persist;
 import org.apache.tapestry5.annotations.Property;
-import org.apache.tapestry5.ioc.annotations.Inject;
 
 import org.chenillekit.demo.components.LeftSideMenu;
 import org.chenillekit.tapestry.core.components.ProtoTube;
@@ -35,34 +32,25 @@ public class ProtoTubeDemo
 	@Property
 	private List<ProtoTubeIdHolder> youtubeIds;
 
-	@Persist
-	@Property
-	private String activePanel;
-
 	@Component(parameters = {"menuName=demo"})
 	private LeftSideMenu menu;
 
 	@Component(parameters = {"youtubeIds=youtubeIds"})
 	private ProtoTube protoTube;
 
-	@Inject
-	private ComponentResources resources;
-
 	/**
-	 * Tapestry page lifecycle method.
-	 * Called when the page is instantiated and added to the page pool.
-	 * Initialize components, and resources that are not request specific.
+	 * Tapestry render phase method.
+	 * Initialize temporary instance variables here.
 	 */
-	void pageLoaded()
+	void setupRender()
 	{
 		youtubeIds = new ArrayList<ProtoTubeIdHolder>();
 		youtubeIds.add(new ProtoTubeIdHolder("NTI6qzaGtuY", "Caracho - In Hamburg sagt man JaJa"));
 		youtubeIds.add(new ProtoTubeIdHolder("bbMYEPecCqY", "Nena, Olli & Remmler - Ich kann nix dafür"));
 		youtubeIds.add(new ProtoTubeIdHolder("hHkhzNLHC4g", "Rammstein & Tatu - Moskau"));
 		youtubeIds.add(new ProtoTubeIdHolder("_k-vPv-XEpg", "Rammstein - Keine Lust"));
-		youtubeIds.add(new ProtoTubeIdHolder("lk06_ll_vgo", "Rammstein - Mein Teil"));
 		youtubeIds.add(new ProtoTubeIdHolder("QHkqIyOtuzU", "Rammstein - Zwitter"));
-		youtubeIds.add(new ProtoTubeIdHolder("eTcPIAewhMg", "Marilyn Manson - Tainted Love"));
+		youtubeIds.add(new ProtoTubeIdHolder("md2UNybl8PY", "Marilyn Manson - Tainted Love"));
 		youtubeIds.add(new ProtoTubeIdHolder("L2GaCnAiuvo", "Depeche Mode - Wrong"));
 		youtubeIds.add(new ProtoTubeIdHolder("eBQBUURLtZU", "Großstadtgeflüster - Haufenweise Scheiße by Jenny"));
 		youtubeIds.add(new ProtoTubeIdHolder("rznn6bgq5nA", "Marilyn Manson - The Beautiful People"));
