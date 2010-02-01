d730498b9f477d224546392054819a5c55043522
Sven Homburgs <homburgs@gmail.com>
2010-02-01 09:30:23 +0000

    actualized dependencies

--- chenillekit-demo/src/main/java/org/chenillekit/demo/services/DemoModule.java
+++ chenillekit-demo/src/main/java/org/chenillekit/demo/services/DemoModule.java
@@ -14,11 +14,7 @@
 
 package org.chenillekit.demo.services;
 
-import java.io.IOException;
-import java.net.URL;
-import java.util.List;
-import java.util.Map;
-
+import freemarker.template.Configuration;
 import org.apache.tapestry5.SymbolConstants;
 import org.apache.tapestry5.ioc.MappedConfiguration;
 import org.apache.tapestry5.ioc.ObjectLocator;
@@ -34,8 +30,6 @@ import org.apache.tapestry5.services.Request;
 import org.apache.tapestry5.services.RequestFilter;
 import org.apache.tapestry5.services.RequestHandler;
 import org.apache.tapestry5.services.Response;
-
-import freemarker.template.Configuration;
 import org.chenillekit.demo.data.Track;
 import org.chenillekit.google.ChenilleKitGoogleConstants;
 import org.chenillekit.image.services.ImageService;
@@ -46,159 +40,174 @@ import org.chenillekit.template.services.impl.FreeMarkerServiceImpl;
 import org.chenillekit.template.services.impl.VelocityServiceImpl;
 import org.slf4j.Logger;
 
+import java.io.IOException;
+import java.net.URL;
+import java.util.List;
+import java.util.Map;
+
 /**
  * @version $Id$
  */
 public class DemoModule
 {
-	public void contributeMasterDispatcher(OrderedConfiguration<Dispatcher> configuration,
-										   ObjectLocator locator)
-	{
-		configuration.add("URIAsset", locator.autobuild(URIDispatcher.class), "after:Asset");
-	}
+    public void contributeMasterDispatcher(OrderedConfiguration<Dispatcher> configuration,
+                                           ObjectLocator locator)
+    {
+        configuration.add("URIAsset", locator.autobuild(URIDispatcher.class), "after:Asset");
+    }
 
-	/**
-	 * Contributes factory defaults that map be overridden.
-	 *
-	 * @param configuration configuration map
-	 */
-	public static void contributeApplicationDefaults(MappedConfiguration<String, String> configuration)
-	{
-		configuration.add(SymbolConstants.PRODUCTION_MODE, "false");
-		configuration.add(SymbolConstants.SUPPORTED_LOCALES, "en,it,de,fr");
-		configuration.add(ChenilleKitGoogleConstants.GOOGLE_REFERER, "www.chenillekit.org");
+    /**
+     * Contributes factory defaults that map be overridden.
+     *
+     * @param configuration configuration map
+     */
+    public static void contributeApplicationDefaults(MappedConfiguration<String, String> configuration)
+    {
+        configuration.add(SymbolConstants.PRODUCTION_MODE, "false");
+        configuration.add(SymbolConstants.SUPPORTED_LOCALES, "en,it,de,fr");
+        configuration.add(ChenilleKitGoogleConstants.GOOGLE_REFERER, "www.chenillekit.org");
 
-		boolean isProductionMode = Boolean.valueOf(System.getProperty(SymbolConstants.PRODUCTION_MODE, "true"));
-		if (isProductionMode)
-			// Google-API Key "www.chenillekit.org"
-			configuration.add(ChenilleKitGoogleConstants.GOOGLE_KEY, "ABQIAAAAi_YFwIW0ZYz1tm9NA5dpjxQdr9K2fBzQ2nv81qbqohyufO_eixS_5MtSz15QdDu7FnDcaswUkcFzOQ");
-		else
-			// Google-API Key "localhost"
-			configuration.add(ChenilleKitGoogleConstants.GOOGLE_KEY, "ABQIAAAAi_YFwIW0ZYz1tm9NA5dpjxQgrEgu6vWt7HL-5aFrx0YLtTRf-hQe7xlPB5qe4SL3L7K1LcW221_now");
-	}
+        boolean isProductionMode = Boolean.valueOf(System.getProperty(SymbolConstants.PRODUCTION_MODE, "true"));
+        if (isProductionMode)
+            // Google-API Key "www.chenillekit.org"
+            configuration.add(ChenilleKitGoogleConstants.GOOGLE_KEY, "ABQIAAAAi_YFwIW0ZYz1tm9NA5dpjxQdr9K2fBzQ2nv81qbqohyufO_eixS_5MtSz15QdDu7FnDcaswUkcFzOQ");
+        else
+            // Google-API Key "localhost"
+            configuration.add(ChenilleKitGoogleConstants.GOOGLE_KEY, "ABQIAAAAi_YFwIW0ZYz1tm9NA5dpjxQgrEgu6vWt7HL-5aFrx0YLtTRf-hQe7xlPB5qe4SL3L7K1LcW221_now");
+    }
 
-	public static MusicLibrary buildMusicLibrary(Logger log)
-	{
-		URL library = DemoModule.class.getResource("iTunes.xml");
+    public static MusicLibrary buildMusicLibrary(Logger log)
+    {
+        URL library = DemoModule.class.getResource("iTunes.xml");
 
-		final List<Track> tracks = new MusicLibraryParser(log).parseTracks(library);
+        final List<Track> tracks = new MusicLibraryParser(log).parseTracks(library);
 
-		final Map<Long, Track> idToTrack = CollectionFactory.newMap();
+        final Map<Long, Track> idToTrack = CollectionFactory.newMap();
 
-		for (Track t : tracks)
-		{
-			idToTrack.put(t.getId(), t);
-		}
+        for (Track t : tracks)
+        {
+            idToTrack.put(t.getId(), t);
+        }
 
-		return new MusicLibrary()
-		{
-			public Track getById(long id)
-			{
-				Track result = idToTrack.get(id);
+        return new MusicLibrary()
+        {
+            public Track getById(long id)
+            {
+                Track result = idToTrack.get(id);
 
-				if (result != null) return result;
+                if (result != null) return result;
 
-				throw new IllegalArgumentException(String.format("No track with id #%d.", id));
-			}
+                throw new IllegalArgumentException(String.format("No track with id #%d.", id));
+            }
 
-			public List<Track> getTracks()
-			{
-				return tracks;
-			}
+            public List<Track> getTracks()
+            {
+                return tracks;
+            }
 
-			public List<Track> findByMatchingTitle(String title)
-			{
-				String titleLower = title.toLowerCase();
+            public List<Track> findByMatchingTitle(String title)
+            {
+                String titleLower = title.toLowerCase();
 
-				List<Track> result = CollectionFactory.newList();
+                List<Track> result = CollectionFactory.newList();
 
-				for (Track t : tracks)
-				{
-					if (t.getTitle().toLowerCase().startsWith(titleLower)) result.add(t);
-				}
+                for (Track t : tracks)
+                {
+                    if (t.getTitle().toLowerCase().startsWith(titleLower)) result.add(t);
+                }
 
-				return result;
-			}
-		};
-	}
+                return result;
+            }
+        };
+    }
 
-	/**
-	 * This is a service definition, the service will be named TimingFilter. The interface,
-	 * RequestFilter, is used within the RequestHandler service pipeline, which is built from the
-	 * RequestHandler service configuration. Tapestry IoC is responsible for passing in an
-	 * appropriate Log instance. Requests for static resources are handled at a higher level, so
-	 * this filter will only be invoked for Tapestry related requests.
-	 *
-	 * @param logger system logger
-	 *
-	 * @return the request filter
-	 */
-	public RequestFilter buildTimingFilter(final Logger logger)
-	{
-		return new RequestFilter()
-		{
-			public boolean service(Request request, Response response, RequestHandler handler)
-					throws IOException
-			{
-				long startTime = System.currentTimeMillis();
+    /**
+     * This is a service definition, the service will be named TimingFilter. The interface,
+     * RequestFilter, is used within the RequestHandler service pipeline, which is built from the
+     * RequestHandler service configuration. Tapestry IoC is responsible for passing in an
+     * appropriate Log instance. Requests for static resources are handled at a higher level, so
+     * this filter will only be invoked for Tapestry related requests.
+     *
+     * @param logger system logger
+     * @return the request filter
+     */
+    public RequestFilter buildTimingFilter(final Logger logger)
+    {
+        return new RequestFilter()
+        {
+            public boolean service(Request request, Response response, RequestHandler handler)
+                    throws IOException
+            {
+                long startTime = System.currentTimeMillis();
 
-				try
-				{
-					// The reponsibility of a filter is to invoke the corresponding method
-					// in the handler. When you chain multiple filters together, each filter
-					// received a handler that is a bridge to the next filter.
+                try
+                {
+                    // The reponsibility of a filter is to invoke the corresponding method
+                    // in the handler. When you chain multiple filters together, each filter
+                    // received a handler that is a bridge to the next filter.
 
-					return handler.service(request, response);
-				}
-				finally
-				{
-					long elapsed = System.currentTimeMillis() - startTime;
+                    return handler.service(request, response);
+                }
+                finally
+                {
+                    long elapsed = System.currentTimeMillis() - startTime;
 
-					logger.info(String.format("Request time: %d ms", elapsed));
-				}
-			}
-		};
-	}
+                    logger.info(String.format("Request time: %d ms", elapsed));
+                }
+            }
+        };
+    }
 
-	/**
-	 * This is a contribution to the RequestHandler service configuration. This is how we extend
-	 * Tapestry using the timing filter. A common use for this kind of filter is transaction
-	 * management or security.
-	 *
-	 * @param configuration ordered configuration
-	 * @param filter		the request filter
-	 */
-	public void contributeRequestHandler(OrderedConfiguration<RequestFilter> configuration,
-										 RequestFilter filter)
-	{
-		// Each contribution to an ordered configuration has a name, When necessary, you may
-		// set constraints to precisely control the invocation order of the contributed filter
-		// within the pipeline.
-		configuration.add("Timing", filter);
-	}
+    /**
+     * This is a contribution to the RequestHandler service configuration. This is how we extend
+     * Tapestry using the timing filter. A common use for this kind of filter is transaction
+     * management or security.
+     *
+     * @param configuration ordered configuration
+     * @param filter        the request filter
+     */
+    public void contributeRequestHandler(OrderedConfiguration<RequestFilter> configuration,
+                                         RequestFilter filter)
+    {
+        // Each contribution to an ordered configuration has a name, When necessary, you may
+        // set constraints to precisely control the invocation order of the contributed filter
+        // within the pipeline.
+        configuration.add("Timing", filter);
+    }
 
-	public static ThumbNailService buildThumbnailService(Logger logger,
-														 ImageService imageService,
-														 Context context,
-														 @InjectService("ContextAssetFactory")AssetFactory assetFactory)
-	{
-		return new ThumbNailServiceImpl(logger, imageService, context, assetFactory);
-	}
+    public static ThumbNailService buildThumbnailService(Logger logger,
+                                                         ImageService imageService,
+                                                         Context context,
+                                                         @InjectService("ContextAssetFactory") AssetFactory assetFactory)
+    {
+        return new ThumbNailServiceImpl(logger, imageService, context, assetFactory);
+    }
 
-	/**
-	 * contribute configuration to velocity template service.
-	 */
-	public static void contributeVelocityService(MappedConfiguration<String, Resource> configuration)
-	{
-		configuration.add(VelocityServiceImpl.CONFIG_RESOURCE_KEY, new ClasspathResource("velocity.properties"));
-	}
+    /**
+     * contribute configuration to velocity template service.
+     */
+    public static void contributeVelocityService(MappedConfiguration<String, Resource> configuration)
+    {
+        configuration.add(VelocityServiceImpl.CONFIG_RESOURCE_KEY, new ClasspathResource("velocity.properties"));
+    }
 
-	/**
-	 * contribute configuration to freemarker template service.
-	 */
-	public static void contributeFreeMarkerService(MappedConfiguration<String, Configuration> configuration)
-	{
-		freemarker.template.Configuration config = new freemarker.template.Configuration();
-		configuration.add(FreeMarkerServiceImpl.CONFIG_RESOURCE_KEY, config);
-	}
+    /**
+     * contribute configuration to freemarker template service.
+     */
+    public static void contributeFreeMarkerService(MappedConfiguration<String, Configuration> configuration)
+    {
+        freemarker.template.Configuration config = new freemarker.template.Configuration();
+        configuration.add(FreeMarkerServiceImpl.CONFIG_RESOURCE_KEY, config);
+    }
+
+    /**
+     * overide the tapestry configuration, that is too restrictive (missing .swf).
+     *
+     * @param regex
+     */
+    public void contributeRegexAuthorizer(org.apache.tapestry5.ioc.Configuration<String> regex)
+    {
+        String pathPattern = "([^/.]+/)*[^/.]+\\.((swf)|(css)|(js)|(jpg)|(jpeg)|(png)|(gif))$";
+        regex.add("^org/chenillekit/demo/" + pathPattern);
+    }
 }
